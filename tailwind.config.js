bc33e20f0219e0540359e21dd65c28972833713e
jinsen-458 <changfuliu525@gmail.com>
2026-09-20 22:50:13 +0800

    Optimize load speed: compiled Tailwind CSS + self-hosted Font Awesome

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#996533',
+        secondary: '#c8a87c',
+        light: '#f8f5f0'
+      }
+    }
+  },
+  plugins: []
+};
